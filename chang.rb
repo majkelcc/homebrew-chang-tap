7588521db48661b0a94b0ad411889665d042d525
Michal Laskowski <m@majkel.cc>
2016-09-05 01:15:51 +0200

    chang v0.1.1

--- chang.rb
+++ chang.rb
@@ -2,8 +2,8 @@ class Chang < Formula
   desc "Chang!"
   homepage "https://github.com/majkelcc/chang"
   url "https://github.com/majkelcc/chang/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "3732594562b66b92f402771533eeb9b551e710f9220c65d7ad74c804deb3f5ff"
+  version "0.1.1"
+  sha256 "9f7fc7fc80ee246528fd05a5fe3c415bd96753eabd4c7e9e8b6938335cb1cfa4"
 
   def install
     bin.install "chang"
