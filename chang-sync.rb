93a6d681c5059198fe649041e1595a362d90c0ae
Michal Laskowski <m@majkel.cc>
2016-09-05 08:48:44 +0200

    chang-sync v0.2.1

--- chang-sync.rb
+++ chang-sync.rb
@@ -1,9 +1,9 @@
 class ChangSync < Formula
   desc "Chang sync"
   homepage "https://github.com/majkelcc/chang-sync"
-  url "https://github.com/majkelcc/chang-sync/archive/0.2.tar.gz"
-  version "0.2"
-  sha256 "2bb5c7035b6232741cbc99d4fae98bd4f18f5815e4553daee897e4e2c6db1b60"
+  url "https://github.com/majkelcc/chang-sync/archive/0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "bade746aa57612d2dd5bf7a8a9bcc58841fbf20f45e8c7e00709c1ffabdc899a"
 
   def install
     bin.install "chang-sync"
