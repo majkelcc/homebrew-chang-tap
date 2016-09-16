dbaaff04ea4c710f55757526ad42289b381fb442
Michal Laskowski <m@majkel.cc>
2016-09-16 13:09:58 +0200

    chang-rev-proxy v0.1.1

--- chang-rev-proxy.rb
+++ chang-rev-proxy.rb
@@ -1,12 +1,14 @@
 class ChangRevProxy < Formula
   desc "nginx rev proxy for chang"
   homepage "https://github.com/majkelcc/chang-rev-proxy"
-  url "https://github.com/majkelcc/chang-rev-proxy/archive/0.1.tar.gz"
-  version "0.1"
-  sha256 "49a03904eace68fe2765b1890faedda992a2444e9d1d9a54549f7978ce079183"
+  url "https://github.com/majkelcc/chang-rev-proxy/archive/0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "6a31471ae759d81e90783a4c781a67361b928fafeeb0977cdf45b233d1f6ef89"
 
   def install
     system 'PATH="/usr/local/bin:$PATH" docker-compose build'
+    inreplace "chang-rev-proxy", "__HOMEBREW_PREFIX__", prefix
+    prefix.install "docker-compose.yml"
     bin.install "chang-rev-proxy"
   end
 end
