ce15efce6242b941f55ce1ffa13b1f2d9e6db68d
Michal Laskowski <m@majkel.cc>
2017-01-10 10:37:18 +0700

    chang-bash-completion v1.1

--- chang-bash-completion.rb
+++ chang-bash-completion.rb
@@ -1,9 +1,9 @@
 class ChangBashCompletion < Formula
   desc "Bash completion for chang"
   homepage "https://github.com/majkelcc/chang-bash-completion"
-  url "https://github.com/majkelcc/chang-bash-completion/archive/1.0.tar.gz"
-  version "1.0"
-  sha256 "e0afd289b298b3d48a8f27f92c58e43f17d0aa0a9ea2dcc3502d1a0253d0f007"
+  url "https://github.com/majkelcc/chang-bash-completion/archive/1.1.tar.gz"
+  version "1.1"
+  sha256 "818355a5fc8742ad3e16997ad85c139b87baf5e051be36c292a9a2dd1f20a0d1"
 
   def install
     bash_completion.install "chang"
