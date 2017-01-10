1b48e99e1be03a2b39f9a12993fdb757c9550a6c
Michal Laskowski <m@majkel.cc>
2017-01-10 10:37:35 +0700

    chang-zsh-completion v1.1

--- chang-zsh-completion.rb
+++ chang-zsh-completion.rb
@@ -1,9 +1,9 @@
 class ChangZshCompletion < Formula
   desc "Zsh completion for chang"
   homepage "https://github.com/majkelcc/chang-zsh-completion"
-  url "https://github.com/majkelcc/chang-zsh-completion/archive/1.0.tar.gz"
-  version "1.0"
-  sha256 "b2c763181f26664baac9532b5affc2adc904dfb8e5f9e68eb772ec7d59e6c411"
+  url "https://github.com/majkelcc/chang-zsh-completion/archive/1.1.tar.gz"
+  version "1.1"
+  sha256 "f0da24f7a0891e01e5a6ff67141099e137f2310252780b4dbb9f1e7fa9b890d1"
 
   def install
     zsh_completion.install "chang"
